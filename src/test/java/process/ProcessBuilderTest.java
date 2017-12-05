35d48fb6340c7d9d4811db1d52b4f5147dc16802
Michał <olszewskimichal@outlook.com>
2017-12-05 19:33:56 +0100

    Dodanie testów - selenium

--- src/test/java/process/ProcessBuilderTest.java
+++ src/test/java/process/ProcessBuilderTest.java
@@ -3,7 +3,6 @@ package process;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
-import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import org.junit.jupiter.api.Test;
@@ -24,5 +23,17 @@ public class ProcessBuilderTest {
     onExit
         .thenAccept(ph -> System.out.println("PID " + ph.pid() + " został zabity"));
     assertThat(handle.isAlive()).isFalse();
+  }
+
+  @Test
+  void shouldStartElasticSeatch() throws IOException, InterruptedException {
+    Process process = new ProcessBuilder("F:\\kibana\\elasticsearch-5.6.1\\elasticsearch-5.6.1\\bin\\elasticsearch.bat").start();
+    System.out.println("elasticSearch wstal jako proces " + process.pid());
+    Thread.sleep(5000);
+    Process process2 = new ProcessBuilder("F:\\kibana\\logstash-5.6.1\\logstash-5.6.1\\bin\\logstash.bat -f logstash-rss.conf").start();
+    System.out.println("logstash wstal jako proces " + process2.pid());
+    Thread.sleep(5000);
+    Process process3 = new ProcessBuilder("F:\\kibana\\kibana-5.6.1-windows-x86\\kibana-5.6.1-windows-x86\\bin\\kibana.bat").start();
+    System.out.println("kibana wstala jako proces " + process3.pid());
   }
 }
