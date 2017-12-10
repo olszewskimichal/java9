f9ead5b2a562037101cb73a5a6d3960ae1c9bd89
Michał <olszewskimichal@outlook.com>
2017-12-10 12:07:00 +0100

    dodanie obsługi Map.ofEntries

--- src/test/java/collectionFactory/MapFactory.java
+++ src/test/java/collectionFactory/MapFactory.java
@@ -1,5 +1,6 @@
 package collectionFactory;
 
+import static java.util.Map.entry;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -27,5 +28,11 @@ class MapFactory {
     Map<String, String> stringMap = Map.of("hello", "world");
 
     assertThrows(UnsupportedOperationException.class, () -> stringMap.put("1", "2"));
+  }
+
+  @Test
+  void shouldCreateMapWithEntries(){
+    Map<String, Integer> entriesMap = Map.ofEntries(entry("aaaa", 1), entry("bbbb", 2));
+    assertThat(entriesMap).isNotEmpty().hasSize(2);
   }
 }
