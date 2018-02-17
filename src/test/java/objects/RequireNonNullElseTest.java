27c924d198afc3b4633c7766bdbe2ad71ff3d793
Michał <olszewskimichal@outlook.com>
2018-02-17 19:25:34 +0100

    drobne poprawki

--- src/test/java/objects/RequireNonNullElseTest.java
+++ src/test/java/objects/RequireNonNullElseTest.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNullElse;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
